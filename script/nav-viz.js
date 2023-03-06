95db3c87d67c3c66644ababf20f1a57999793f3e
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-03-07 02:15:02 +0800

    Adapt theme of navigation pane to website color scheme

--- script/nav-viz.js
+++ script/nav-viz.js
@@ -1,5 +1,5 @@
 $(function () {
-	$('#viz-div').hide();
+	$('tableau-viz').hide();
 
 	const navViz = new Map();
 	const navTitle = new Map();
@@ -8,6 +8,7 @@ $(function () {
 	navViz.set('allergy-symptoms', 'https://public.tableau.com/views/AllergySymptoms_16775975678240/Dashboard-Allergens?:language=en-US&:display_count=n&:origin=viz_share_link');
 
 	navTitle.set('allergy-allergens', 'Allergens');
+	navTitle.set('allergy-symptoms', 'Allergy Symptoms');
 
 	for (const navId of navViz.keys()) {
 		$(`#${navId}`).on('click', function () {
@@ -15,10 +16,11 @@ $(function () {
 				$(this).css('font-weight', 'normal');
 			});
 
-			$('#viz-div').show();
-			$('#project-desc').hide();
+			$('tableau-viz').show();
+			$('#project-desc').remove();
 
 			$(`#${navId}`).css('font-weight', 'bold');
+			$('#viz-title').text(navTitle.get(navId));
 			$('tableau-viz').prop('src', navViz.get(navId));
 		});
 	}
