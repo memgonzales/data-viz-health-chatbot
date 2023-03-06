c913d3fa39542d0d7ae03644e245930e3602b326
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-03-07 02:31:45 +0800

    Hide latency in display loading

--- script/nav-viz.js
+++ script/nav-viz.js
@@ -1,14 +1,17 @@
 $(function () {
 	$('tableau-viz').hide();
+	$('#project-desc').show();
 
 	const navViz = new Map();
 	const navTitle = new Map();
 
 	navViz.set('allergy-allergens', 'https://public.tableau.com/views/Allergy_16770827471540/Dashboard-Allergens?:language=en-US&:display_count=n&:origin=viz_share_link');
 	navViz.set('allergy-symptoms', 'https://public.tableau.com/views/AllergySymptoms_16775975678240/Dashboard-Allergens?:language=en-US&:display_count=n&:origin=viz_share_link');
+	navViz.set('allergy-remedies', 'https://public.tableau.com/views/AllergyRemedies_16775982959400/Dashboard-Remedies?:language=en-US&:display_count=n&:origin=viz_share_link');
 
 	navTitle.set('allergy-allergens', 'Allergens');
 	navTitle.set('allergy-symptoms', 'Allergy Symptoms');
+	navTitle.set('allergy-remedies', 'Allergy Remedies');
 
 	for (const navId of navViz.keys()) {
 		$(`#${navId}`).on('click', function () {
@@ -17,7 +20,7 @@ $(function () {
 			});
 
 			$('tableau-viz').show();
-			$('#project-desc').remove();
+			$('#project-desc').hide();
 
 			$(`#${navId}`).css('font-weight', 'bold');
 			$('#viz-title').text(navTitle.get(navId));
