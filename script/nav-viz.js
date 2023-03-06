f2223cf7ae4611326145bda7334e9b66ffd2a34d
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-03-07 02:49:27 +0800

    Embed the visualizations for buto-muscle

--- script/nav-viz.js
+++ script/nav-viz.js
@@ -9,9 +9,13 @@ $(function () {
 	navViz.set('allergy-symptoms', 'https://public.tableau.com/views/AllergySymptoms_16775975678240/Dashboard-Allergens?:language=en-US&:display_count=n&:origin=viz_share_link');
 	navViz.set('allergy-remedies', 'https://public.tableau.com/views/AllergyRemedies_16775982959400/Dashboard-Remedies?:language=en-US&:display_count=n&:origin=viz_share_link');
 
+	navViz.set('buto-muscle-reliefs', 'https://public.tableau.com/views/ButoMuscleReliefs_16776927356130/Dashboard-Relief?:language=en-US&:display_count=n&:origin=viz_share_link');
+
 	navTitle.set('allergy-allergens', 'Allergens');
 	navTitle.set('allergy-symptoms', 'Allergy Symptoms');
 	navTitle.set('allergy-remedies', 'Allergy Remedies');
+
+	navTitle.set('buto-muscle-reliefs', 'Reliefs for Muscle Pain');
 
 	for (const navId of navViz.keys()) {
 		$(`#${navId}`).on('click', function () {
