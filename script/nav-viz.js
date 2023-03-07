c94fe0742f8c02fcc6024925fc53e16919b196cf
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2023-03-07 10:37:23 +0800

    Refactor script for modularity

--- script/nav-viz.js
+++ script/nav-viz.js
@@ -9,36 +9,29 @@ $(function () {
 
 	for (const [navId, navDetails] of Object.entries(navViz)) {
 		$(`#${navId}`).on('click', function () {
-			$('.viz > li').each(function () {
-				$(this).css('font-weight', 'normal');
-			});
-
-			$('tableau-viz').show();
-			$('#project-desc').hide();
-
-			$(`#${navId}`).css('font-weight', 'bold');
-			$('#viz-title').text(navViz[navId]['title']);
-			$('tableau-viz').prop('src', navViz[navId]['url']);
+			changeViz(navId);
 		});
 	}
 
 	$('#website-title').on('click', function () {
-		$('tableau-viz').hide();
-		$('#project-desc').show();
-		$('#viz-title').text(navTitle.get('project'));
-
-		$('.viz > li').each(function () {
-			$(this).css('font-weight', 'normal');
-			$('.collapse').collapse('hide');
-		});
+		displayProjectInfo();
 	});
 
 	function populateNavViz(navViz) {
+		populateProjectInfo();
+		populateNavAllergy();
+		populateNavButoMuscle();
+		populateCoughColds();
+	}
+
+	function populateProjectInfo() {
 		navViz['project'] = {
 			url: null,
 			title: 'Development of Multi-lingual Chatbot for Health Monitoring of Public School Children'
 		};
+	}
 
+	function populateNavAllergy() {
 		navViz['allergy-allergens'] = {
 			url: 'https://public.tableau.com/views/Allergy_16770827471540/Dashboard-Allergens?:language=en-US&:display_count=n&:origin=viz_share_link',
 			title: 'Allergens'
@@ -53,21 +46,53 @@ $(function () {
 			url: 'https://public.tableau.com/views/AllergyRemedies_16775982959400/Dashboard-Remedies?:language=en-US&:display_count=n&:origin=viz_share_link',
 			title: 'Allergy Remedies'
 		};
+	}
 
-		// navViz.set('buto-muscle-reliefs', 'https://public.tableau.com/views/ButoMuscleReliefs_16776927356130/Dashboard-Relief?:language=en-US&:display_count=n&:origin=viz_share_link');
+	function populateNavButoMuscle() {
+		navViz['buto-muscle-reliefs'] = {
+			url: 'https://public.tableau.com/views/ButoMuscleReliefs_16776927356130/Dashboard-Relief?:language=en-US&:display_count=n&:origin=viz_share_link',
+			title: 'Reliefs for Muscle Pain'
+		};
+	}
 
-		// navViz.set('cough-colds-cough-details', 'https://public.tableau.com/views/CoughColdCoughDetails/Dashboard-CoughDetails?:language=en-US&:display_count=n&:origin=viz_share_link');
-		// navViz.set('cough-colds-fever-details', 'https://public.tableau.com/views/CoughColdFeverDetails/Dashboard-FeverDetails?:language=en-US&:display_count=n&:origin=viz_share_link');
-		// navViz.set('cough-colds-medication', 'https://public.tableau.com/views/CoughColdReliefandMedication/Dashboard-ReliefandMedication?:language=en-US&:display_count=n&:origin=viz_share_link');
+	function populateCoughColds() {
+		navViz['cough-colds-cough-details'] = {
+			url: 'https://public.tableau.com/views/CoughColdCoughDetails/Dashboard-CoughDetails?:language=en-US&:display_count=n&:origin=viz_share_link',
+			title: 'Cough Details'
+		};
 
-		// navTitle.set('allergy-allergens', 'Allergens');
-		// navTitle.set('allergy-symptoms', 'Allergy Symptoms');
-		// navTitle.set('allergy-remedies', 'Allergy Remedies');
+		navViz['cough-colds-fever-details'] = {
+			url: 'https://public.tableau.com/views/CoughColdFeverDetails/Dashboard-FeverDetails?:language=en-US&:display_count=n&:origin=viz_share_link',
+			title: 'Fever Details'
+		};
 
-		// navTitle.set('buto-muscle-reliefs', 'Reliefs for Muscle Pain');
+		navViz['cough-colds-medication'] = {
+			url: 'https://public.tableau.com/views/CoughColdReliefandMedication/Dashboard-ReliefandMedication?:language=en-US&:display_count=n&:origin=viz_share_link',
+			title: 'Cough and Colds Medication'
+		};
+	}
 
-		// navTitle.set('cough-colds-cough-details', 'Cough Details');
-		// navTitle.set('cough-colds-fever-details', 'Fever Details');
-		// navTitle.set('cough-colds-medication', 'Cough and Colds Medication');
+	function changeViz(navId) {
+		$('.viz > li').each(function () {
+			$(this).css('font-weight', 'normal');
+		});
+
+		$('tableau-viz').show();
+		$('#project-desc').hide();
+
+		$(`#${navId}`).css('font-weight', 'bold');
+		$('#viz-title').text(navViz[navId]['title']);
+		$('tableau-viz').prop('src', navViz[navId]['url']);
+	}
+
+	function displayProjectInfo() {
+		$('tableau-viz').hide();
+		$('#project-desc').show();
+		$('#viz-title').text(navViz['project']['title']);
+
+		$('.viz > li').each(function () {
+			$(this).css('font-weight', 'normal');
+			$('.collapse').collapse('hide');
+		});
 	}
 });
